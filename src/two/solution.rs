272a15a1bdf5d270d6d70cae43311c27d1894e29
Forrest Dunlap <forrestmdunlap@gmail.com>
2023-12-14 13:12:47 -0500

    Solution 2.2 + refactor

--- src/two/solution.rs
+++ src/two/solution.rs
@@ -1,8 +1,9 @@
+use std::cmp::max;
 use std::path::PathBuf;
 
 use crate::{error::SolutionError, util::load_file, util::SolutionPart};
 
-struct Draw {
+struct CubeSet {
     red: i32,
     green: i32,
     blue: i32,
@@ -10,7 +11,14 @@ struct Draw {
 
 struct Game {
     index: i32,
-    draws: Vec<Draw>,
+    draws: Vec<CubeSet>,
+}
+
+fn extract_rolls(draw: &str, color: &str) -> Result<i32, SolutionError> {
+    match draw.replace(color, "").trim().parse::<i32>() {
+        Ok(drawn_color) => Ok(drawn_color),
+        Err(_) => Err(SolutionError::NoPossibleSolution),
+    }
 }
 
 fn extract_game_parts(game_str: &str) -> Result<Game, SolutionError> {
@@ -21,7 +29,7 @@ fn extract_game_parts(game_str: &str) -> Result<Game, SolutionError> {
         .replace("Game ", "")
         .parse::<i32>()
         .unwrap();
-    let mut draws: Vec<Draw> = vec![];
+    let mut draws: Vec<CubeSet> = vec![];
 
     for draw_set in split_game_str.nth(0).unwrap().split(";") {
         let mut red = 0;
@@ -31,28 +39,14 @@ fn extract_game_parts(game_str: &str) -> Result<Game, SolutionError> {
         for draw in draw_set.split(',') {
             let draw = draw.trim();
             if draw.ends_with("red") {
-                if let Ok(drawn_reds) = draw.replace(" red", "").parse::<i32>() {
-                    red = drawn_reds;
-                } else {
-                    return Err(SolutionError::NoPossibleSolution);
-                }
-            }
-            if draw.ends_with("green") {
-                if let Ok(drawn_reds) = draw.replace(" green", "").parse::<i32>() {
-                    green = drawn_reds;
-                } else {
-                    return Err(SolutionError::NoPossibleSolution);
-                }
-            }
-            if draw.ends_with("blue") {
-                if let Ok(drawn_reds) = draw.replace(" blue", "").parse::<i32>() {
-                    blue = drawn_reds;
-                } else {
-                    return Err(SolutionError::NoPossibleSolution);
-                }
+                red = extract_rolls(draw, "red")?;
+            } else if draw.ends_with("green") {
+                green = extract_rolls(draw, "green")?;
+            } else if draw.ends_with("blue") {
+                blue = extract_rolls(draw, "blue")?;
             }
         }
-        draws.push(Draw { red, green, blue })
+        draws.push(CubeSet { red, green, blue })
     }
 
     return Ok(Game {
@@ -70,6 +64,20 @@ fn game_is_valid(game: &Game) -> bool {
     return true;
 }
 
+fn game_cubeset_power(game: &Game) -> i32 {
+    let mut min_cube_set = CubeSet {
+        red: 0,
+        green: 0,
+        blue: 0,
+    };
+    for draw in game.draws.as_slice() {
+        min_cube_set.red = max(min_cube_set.red, draw.red);
+        min_cube_set.green = max(min_cube_set.green, draw.green);
+        min_cube_set.blue = max(min_cube_set.blue, draw.blue);
+    }
+    min_cube_set.red * min_cube_set.green * min_cube_set.blue
+}
+
 pub fn run(data_path: PathBuf, solution_part: SolutionPart) -> Result<i32, SolutionError> {
     let contents = load_file(data_path)?;
     let mut puzzle_answer: i32 = 0;
@@ -83,7 +91,9 @@ pub fn run(data_path: PathBuf, solution_part: SolutionPart) -> Result<i32, Solut
                     puzzle_answer += game.index;
                 }
             }
-            SolutionPart::PartTwo => todo!(),
+            SolutionPart::PartTwo => {
+                puzzle_answer += game_cubeset_power(&game);
+            }
         }
     }
 
