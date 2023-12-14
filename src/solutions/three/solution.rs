2f5db0ecb313a906360ed034c43299adeb41ae89
Forrest Dunlap <forrestmdunlap@gmail.com>
2023-12-14 13:42:15 -0500

    shadow unused vars in three::solution

--- src/solutions/three/solution.rs
+++ src/solutions/three/solution.rs
@@ -3,5 +3,7 @@ use std::path::PathBuf;
 use crate::{error::SolutionError, util::SolutionPart};
 
 pub fn run(data_path: PathBuf, solution_part: SolutionPart) -> Result<i32, SolutionError> {
+    let _ = solution_part;
+    let _ = data_path;
     Ok(0)
 }
