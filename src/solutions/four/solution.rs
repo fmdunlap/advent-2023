01ddc233a14b1b6534f3a7d84eeb92cad2b8063d
Forrest Dunlap <forrestmdunlap@gmail.com>
2023-12-16 21:11:11 -0500

    Unoptimized 4.1 and 4.2

--- src/solutions/four/solution.rs
+++ src/solutions/four/solution.rs
@@ -1,22 +1,112 @@
-use std::path::PathBuf;
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    fmt::Display,
+    path::PathBuf,
+};
 
 use crate::{
     error::SolutionError,
     util::{load_file, SolutionPart},
 };
 
-pub fn run(data_path: PathBuf, solution_part: SolutionPart) -> Result<i32, SolutionError> {
-    let content = load_file(data_path)?;
-    match solution_part {
-        SolutionPart::PartOne => Ok(part_one_solution(content)),
-        SolutionPart::PartTwo => Ok(part_two_solution(content)),
+struct ScratchCard {
+    winning_numbers: HashSet<u32>,
+    contained_numbers: Vec<u32>,
+    num_winning_numbers: u32,
+    points: u32,
+}
+
+impl From<&str> for ScratchCard {
+    fn from(value: &str) -> Self {
+        let mut winning_numbers: HashSet<u32> = HashSet::new();
+        let mut contained_numbers: Vec<u32> = vec![];
+
+        let card_numbers = value.split_once(':').unwrap().1.split_once('|').unwrap();
+
+        for winning_number in card_numbers.0.trim().split(' ') {
+            if let Ok(num) = winning_number.trim().parse::<u32>() {
+                winning_numbers.insert(num);
+            }
+        }
+        for contained_number in card_numbers.1.trim().split(' ') {
+            if let Ok(num) = contained_number.trim().parse::<u32>() {
+                contained_numbers.push(num)
+            }
+        }
+
+        let num_winning_numbers = contained_numbers
+            .iter()
+            .filter(|cn| winning_numbers.contains(cn))
+            .count() as u32;
+
+        let points: u32 = match num_winning_numbers {
+            0 => 0,
+            _ => 2_u32.pow(num_winning_numbers as u32 - 1),
+        };
+
+        ScratchCard {
+            winning_numbers,
+            contained_numbers,
+            num_winning_numbers,
+            points,
+        }
     }
 }
 
-fn part_two_solution(content: String) -> i32 {
-    todo!()
+impl Display for ScratchCard {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let winning_num_strs: Vec<String> = self
+            .winning_numbers
+            .iter()
+            .map(|num| num.to_string())
+            .collect();
+        let contained_num_strs: Vec<String> = self
+            .contained_numbers
+            .iter()
+            .map(|num| num.to_string())
+            .collect();
+
+        write!(
+            f,
+            "Winning: {}\nContained: {}",
+            winning_num_strs.join(","),
+            contained_num_strs.join(",")
+        )
+    }
 }
 
-fn part_one_solution(content: String) -> i32 {
-    todo!()
+pub fn run(data_path: PathBuf, solution_part: SolutionPart) -> Result<i32, SolutionError> {
+    let content = load_file(data_path)?;
+    let mut cards: Vec<ScratchCard> = vec![];
+    for line in content.split('\n') {
+        let card = ScratchCard::from(line);
+        cards.push(card);
+    }
+
+    match solution_part {
+        SolutionPart::PartOne => Ok(part_one_solution(cards)),
+        SolutionPart::PartTwo => Ok(part_two_solution(cards)),
+    }
+}
+
+fn part_two_solution(cards: Vec<ScratchCard>) -> i32 {
+    let card_map: HashMap<usize, &ScratchCard> = cards
+        .iter()
+        .enumerate()
+        .map(|entry| (entry.0, entry.1))
+        .collect();
+    let mut card_deque: VecDeque<(usize, &ScratchCard)> = cards.iter().enumerate().collect();
+    let mut answer = 0;
+    while let Some((index, card)) = card_deque.pop_front() {
+        answer += 1;
+        for i in 1..card.num_winning_numbers + 1 {
+            let new_card_index = index + i as usize;
+            card_deque.push_back((new_card_index, card_map[&new_card_index]))
+        }
+    }
+    answer
+}
+
+fn part_one_solution(cards: Vec<ScratchCard>) -> i32 {
+    cards.iter().map(|card| card.points).sum::<u32>() as i32
 }
